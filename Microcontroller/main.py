a2e3cad5ca841f85a8430cbf038b2e7cc1b8a16a
Nora <nyw@andrew.cmu.edu>
2023-05-06 15:18:57 -0400

    new changes

--- Microcontroller/main.py
+++ Microcontroller/main.py
@@ -4,7 +4,7 @@ from xml_parse import *
 from pin_mapping import *
 
 import time
-import sys
+import os
 import serial
 
 # Macros
@@ -12,8 +12,8 @@ DEBUG = True
 RPI_CONNECTED = True
 RPI_SER_PORT = '/dev/serial0'
 DEFAULT_BAUD = 115200
-MAX_LATENCY = 0.150 # in seconds
-XML_FILES_PATH = "/home/team-d7/d7-accompanyBot/XMLFiles/"
+MAX_LATENCY = 10 # in seconds
+XML_FILES_PATH = "/home/pi/d7-accompanyBot/XMLFiles/"
 DEFAULT_OCTAVE = 3
 MAX_NOTES = 5
 
@@ -22,7 +22,7 @@ def isNoteInRange(octave):
 
 def playNotes(setOfNotes, currentOctave):
     """Plays the notes in a list of notes
-    
+
     Args: currentMeasure (int): The time step to retreive notes for
     """
     print(setOfNotes)
@@ -31,7 +31,7 @@ def playNotes(setOfNotes, currentOctave):
         octave = note.octave
         state = note.state
         print(pitch)
-        
+
         pin = noteToPinDict.get(pitch, "NO PIN")
         if (pin == "NO PIN"):
             print("ERROR: Not a valid pitch\n")
@@ -48,9 +48,9 @@ def playNotes(setOfNotes, currentOctave):
                 if (pitch in currentlyPlaying):
                     currentlyPlaying.remove(pitch)
             if (RPI_CONNECTED): pi.write(pin, state)
-    
+
     if (DEBUG): print(currentlyPlaying)
-    
+
 def stopPlaying():
     if (RPI_CONNECTED): pi.clear_bank_1(POSSIBLE_PINS)
     print(bin(pi.read_bank_1() & POSSIBLE_PINS))
@@ -83,6 +83,8 @@ ser = serial.Serial(RPI_SER_PORT, DEFAULT_BAUD, timeout=MAX_LATENCY)
 
 pi = setUpPins()
 
+pi.clear_bank_1(POSSIBLE_PINS)
+
 # Initializing variables
 scheduledPiece = dict()
 startTime = time.time_ns()
@@ -100,7 +102,7 @@ fileLoaded = False
 measureDuration_ns = 0
 
 # Run scheduling
-#(tempoInfo, totalMeasures, scheduledPiece, currentOctave) = schedule("/home/team-d7/d7-accompanyBot/Microcontroller/MetronomeTest60bpm.xml", scheduledPiece)
+#(tempoInfo, totalMeasures, scheduledPiece, currentOctave) = schedule("/home/pi/d7-accompanyBot/XMLFiles/ChromaticCscale.xml", scheduledPiece)
 #if (tempoInfo.tempoValue > tempoInfo.maxTempo):
 #    print(f"ERROR: Parsed tempo of {tempoInfo.tempoValue} exceeds max tempo. Playing with max tempo of {tempoInfo.maxTempo}")
 #    tempoInfo.tempoValue = tempoInfo.maxTempo
@@ -109,50 +111,44 @@ measureDuration_ns = 0
 #measureDuration_ns = tempoInfo.getMeasureDuration_ns()
 #if (DEBUG): print(scheduledPiece)
 
-lightCounter = 0
+print("Starting loop")
+
 # Golden Loop
 while(True):
-    # try and except to catch errors 
+    # try and except to catch errors
     try:
-        state = lightCounter % 2
-        pi.write(PIN0, state)
-        lightCounter += 1
-        time.sleep(0.5)
-
         command = checkSerialInput()
         if (command != None):
             # Set Parameters based on received command
-            
-            # Play 
+
+            # Play
             if (command == "S"):
                 if (DEBUG): print("Start Playing")
-                
+
                 if (paused and fileLoaded):
                     paused = False
                     justStarted = True
                     startMeasure = currentMeasure
                     startTime = time.time_ns()
-                    
-                    print("received start")
-                    ser.write("L111\n".encode())
 
-            # Pause   
+                    print("received start")
+
+            # Pause
             elif (command == "P"):
                 if (fileLoaded):
                     if (DEBUG): print("Pause Playing")
                     stopPlaying()
                     paused = True
-                
-                ser.write("L111\n".encode())
+
                 print("received pause")
-            
+
             # Change the current measure
             elif (command[0] == "C" and len(command) > 1):
                 if (fileLoaded):
                     measure = command[1:] # Removes the first character
-                    
+
                     if (DEBUG): print("Parsed measure: " + measure)
-                    
+
                     startMeasure = int(measure)
                     currentMeasure = startMeasure
                     justStarted = True
@@ -175,30 +171,31 @@ while(True):
                     startTime = time.time_ns()
 
             # File received
-            elif (command[0] == "F"  and len(command) > 1): 
+            elif (command[0] == "F"  and len(command) > 1):
                 file = command[1:] # Removes the first character
-                
+
                 filepath = XML_FILES_PATH + file
-                
+
                 print(filepath)
-                
+
                 # Check that file exists
-                try: 
+                try:
                     open(filepath) # To catch the exception for if the file doesn't exist
                     fileLoaded = True
+                    newScheduledPiece = dict()
                     (tempoInfo, totalMeasures, newScheduledPiece, currentOctave) = schedule(filepath, scheduledPiece)
-                    
+
                     # Check that tempo is not too high
                     print("Max Tempo: " + str(tempoInfo.maxTempo))
                     if (tempoInfo.tempoValue > tempoInfo.maxTempo):
                         print(f"ERROR: Parsed tempo of {tempoInfo.tempoValue} exceeds max tempo. Playing with max tempo of {tempoInfo.maxTempo}")
                         tempoInfo.tempoValue = tempoInfo.maxTempo
-                    
+
                     ser.write(("N" + str(totalMeasures) + "\n").encode())
                     ser.write(("M" + str(tempoInfo.maxTempo) + "\n").encode())
                     ser.write(("O" + str(currentOctave) + "\n").encode())
                     ser.write(("C0\n").encode())
-                    
+
                     scheduledPiece = newScheduledPiece
                     measureDuration_ns = tempoInfo.getMeasureDuration_ns()
 
@@ -210,25 +207,25 @@ while(True):
                     currentlyPlaying = set()
                     offsetList = []
                     paused = True
-                except: 
+                except:
                     print("ERROR: File not found")
         else:
             if (not paused and currentMeasure <= totalMeasures):
                 newMeasure = getMeasureFromTime()
                 if (justStarted or newMeasure > currentMeasure):
                     if (DEBUG): print("Measure: {}".format(newMeasure))
-                    
+
                     justStarted = 0
                     currentMeasure = newMeasure
                     notesToPlay = scheduledPiece.get(currentMeasure, "none")
                     if (notesToPlay != "none"):
                         offsetList = list(notesToPlay.keys())
                         offsetList.sort()
-                    
+
                     # Send new measure number over serial port to computer
                     writeData = "C" + str(currentMeasure) + "\n"
                     if (RPI_CONNECTED): ser.write(writeData.encode())
-                
+
                 newOffset = getCurrentOffset()
                 # print("offset" + str(newOffset))
                 if (len(offsetList) > 0 and newOffset > offsetList[0]):
